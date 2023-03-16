cce6bb42e38b1f40ef3a94e11bff62d9d47ede1c
nithinacharya11 <nithinpacharya@gmail.com>
2023-03-16 12:24:05 +0530

    first commit

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Layout = ({children}) => {
+  return (
+    <div>
+      <Nav/>
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
